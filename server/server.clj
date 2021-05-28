18fdb2b6a01744a2c9793499afbce56993284c28
Mauricio <mauricio@pop-os.localdomain>
2021-05-27 22:05:03 -0300

    Puta merda funciona dsesgraçaaaaaaaaaaaaaaaaaa

---
+++ server/server.clj
@@ -0,0 +1,44 @@
+(ns calc-api.server.server
+  (:require [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :as params]
+            [ring.util.http-response :as response]
+            [reitit.ring.middleware.muuntaja :as muuntaja]
+            [muuntaja.core :as m]
+            [reitit.ring.coercion :as coercion]
+            [reitit.ring :as ring]))
+
+(def app
+  (ring/ring-handler
+   (ring/router
+    [hello-routes]
+    {:data {:muuntaja m/instance
+            :middleware [params/wrap-params
+                         muuntaja/format-middleware
+                         coercion/coerce-exceptions-middleware
+                         coercion/coerce-request-middleware
+                         coercion/coerce-response-middleware]}})
+   (ring/create-default-handler)))
+
+
+
+(def hello-routes
+  ["/hello" {:get {:handler (fn [_]
+                              (response/ok {:mesage "Hello Reitit!"}))}}])
+
+
+
+(defonce running-server (atom nil))
+
+(defn start
+  []
+  (when (nil? @running-server)
+    (reset! running-server (jetty/run-jetty #'app {:port  3000
+                                                   :join? false})))
+  (println "Server running in port 3000"))
+
+(defn stop
+  []
+  (when-not (nil? @running-server)
+    (.stop @running-server)
+    (reset! running-server nil))
+  (println "Server stopped"))
